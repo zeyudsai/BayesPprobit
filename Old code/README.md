bfaf305b43a47564550c12ae257f9feafa3670c9
Zeyu Ding <zeyu.ding@tu-dortmund.de>
2024-11-22 18:52:01 +0100

    necessary change

--- Old code/README.md
+++
@@ -1,2 +0,0 @@
-# BayesPprobit
-This is the code used for the Paper Scalable Bayesian $p$-Generalized Probit and Logistic Regression 
--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(BayesPProbit)
+library(BayesPprobit)
 
-test_check("BayesPProbit")
+test_check("BayesPprobit")
